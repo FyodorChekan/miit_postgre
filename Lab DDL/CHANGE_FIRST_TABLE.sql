078f59eb6bb03fffcec4091d8de11ed73b3b20b9
FyodorChekan <chekan.fyod@yandex.ru>
2024-03-26 17:06:04 +0300

    Написана первая часть скриптов по лабораторной работе DDL

---
+++ Lab DDL/CHANGE_FIRST_TABLE.sql
@@ -0,0 +1,3 @@
+--Создать два новых файла со скриптами для изменений таблиц.
+alter table "currency"
+    alter column "ID" set not null;
---
+++ Lab DDL/CHANGE_SECOND_TABLE.sql
@@ -0,0 +1,3 @@
+--Создать два новых файла со скриптами для изменений таблиц.
+alter table "balance"
+    alter column "ID_Currency" set not null;
